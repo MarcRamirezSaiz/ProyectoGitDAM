3a170a6b3cbcfe42ed4d2116ab77798a3f3830a1
marramsai <marramsai@ORM4-10375403.mshome.net>
2026-02-05 18:52:11 +0100

    Segundo commit: modificación del mensaje de salida

--- src/es/dam/git/HolaGit.java
+++ src/es/dam/git/HolaGit.java
@@ -4,6 +4,7 @@ public class HolaGit {
 
 	public static void main(String[] args) {
 		System.out.println("¡Hola desde Git!");
+		System.out.println("Este es mi primer proyecto con control de versiones");
 
 	}
 
